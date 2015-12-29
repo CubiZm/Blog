016e8361c53f9c0ae7eb5b9181c83daf4a33c62c
CubiZm <seo-xcube@yandex.ru>
2015-12-29 12:06:34 +0300

    start. styling menu

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var jade = require('gulp-jade');
+var browserSync = require('browser-sync').create();
+
+gulp.task("style", function() {
+	return gulp.src("sass/style.{sass,scss}")
+		.pipe(plumber())
+		.pipe(sass({
+			outputStyle: 'expanded'
+		})).on('error', sass.logError)
+		.pipe(postcss([
+			autoprefixer({browsers: "last 2 versions"})
+		]))
+		.pipe(gulp.dest("css"))
+		.pipe(browserSync.stream());
+});
+
+
+gulp.task('jade', function() {
+
+	gulp.src('./jade/*.jade')
+		.pipe(jade())
+		.pipe(gulp.dest('./'))
+		gulp.watch("jade/*.jade"), ["jade"];
+});
+
+gulp.task("start", function() {
+	gulp.watch("sass/**/*.{sass,scss}", ["style"]);
+	gulp.watch("jade/*.jade"), ["jade"];
+	});
+
+// Static Server + watching scss/html files
+gulp.task('server', ['style'], function() {
+
+		browserSync.init({
+				server: "./",
+				open: false
+		});
+
+		gulp.watch("sass/**/*.scss", ['style']);
+		gulp.watch("jade/*.jade"), ["jade"];
+		gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['serve']);
+
+
