6ddc6d5c3a2a7d7dfb17876f9f1f11fd19ae27dd
CubiZm <seo-xcube@yandex.ru>
2016-01-04 22:12:08 +0300

    create build-project

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,25 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+// var cmq = require('gulp-combine-media-queries');
+var combineMq = require("gulp-combine-mq");
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var rimraf = require('rimraf');
 var jade = require('gulp-jade');
 var browserSync = require('browser-sync').create();
 
+var scriptList = [
+	'source/js/common.js'
+]
+
+gulp.task("build",["style", "images", "script", "html", "fonts"]);
+
 gulp.task("style", function() {
-	return gulp.src("sass/style.{sass,scss}")
+	return gulp.src("source/sass/style.{sass,scss}")
 		.pipe(plumber())
 		.pipe(sass({
 			outputStyle: 'expanded'
@@ -17,10 +31,54 @@ gulp.task("style", function() {
 		.pipe(postcss([
 			autoprefixer({browsers: "last 2 versions"})
 		]))
-		.pipe(gulp.dest("css"))
+		.pipe(combineMq({
+			beautify: false
+		}))
+		.pipe(minifyCss())
+		.pipe(rename({
+				suffix: ".min"
+		}))
+		.pipe(gulp.dest("build/css"))
 		.pipe(browserSync.stream());
 });
 
+gulp.task("images", function() {
+	return gulp.src("source/img/*.{png,jpg,gif,svg}")
+		.pipe(imagemin())
+		.pipe(gulp.dest("build/img"));
+});
+
+gulp.task('script', function() {
+	return gulp.src(scriptList)
+		.pipe(concat('common.js'))
+		.pipe(gulp.dest('./build/js/'))
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("html", function() {
+	return gulp.src("source/jade/*.jade")
+		.pipe(jade())
+		.pipe(gulp.dest("build/"));
+});
+
+gulp.task("fonts", function() {
+	return gulp.src("./source/fonts/*.otf")
+		.pipe(gulp.dest("build/fonts/"));
+});
+
+gulp.task('clean', function (cb) {
+return rimraf('build', cb);
+});
+
+
+gulp.task("start", function() {
+	gulp.watch("sass/**/*.{sass,scss}", ["style"]);
+	gulp.watch("jade/*.jade"), ["jade"];
+	});
 
 gulp.task('jade', function() {
 
@@ -29,11 +87,6 @@ gulp.task('jade', function() {
 		.pipe(gulp.dest('./'))
 		// gulp.watch("jade/*.jade"), ["jade"];
 });
-
-gulp.task("start", function() {
-	gulp.watch("sass/**/*.{sass,scss}", ["style"]);
-	gulp.watch("jade/*.jade"), ["jade"];
-	});
 
 // Static Server + watching scss/html files
 gulp.task('server', ['style'], function() {
